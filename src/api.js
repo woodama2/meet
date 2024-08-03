918817a808f39f55981f0c101cbcf1f3bf60ad69
Mandy <156150059+woodama2@users.noreply.github.com>
2024-08-02 20:40:19 -0400

    AppShell Caching

--- src/api.js
+++ src/api.js
@@ -32,6 +32,12 @@ export const getEvents = async () => {
     return mockData;
   }
 
+  if (!navigator.onLine) {
+    const events = localStorage.getItem('lastEvents');
+    NProgress.done();
+    return events ? JSON.parse(events) : [];
+  }
+
   const token = await getAccessToken();
 
   if (token) {
@@ -44,6 +50,8 @@ export const getEvents = async () => {
     const response = await fetch(url);
     const result = await response.json();
     if (result) {
+      NProgress.done();
+      localStorage.setItem('lastEvents', JSON.stringify(result.events));
       return result.events;
     } else {
       console.error('No result from API'); // Added logging
